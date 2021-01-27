ede50f6dfe721a3325398832bbc39b968762182b
Chenkaiwei <1035759181@qq.com>
2021-01-27 14:15:40 +0800

    Feat: 接入微前端配置

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,23 @@
+const { name } = require('./package')
 
 module.exports = {
   devServer: {
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
     open: process.platform === 'darwin',
     host: 'localhost',
-    port: 3000,
+    port: 8081,
     https: false,
     hotOnly: false,
     proxy: null, // 设置代理
     before: app => {}
+  },
+  configureWebpack: {
+    output: {
+      library: `${name}-[name]`,
+      libraryTarget: 'umd', // 把微应用打包成 umd 库格式
+      jsonpFunction: `webpackJsonp_${name}`
+    }
   }
 }
